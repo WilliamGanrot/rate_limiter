9ed49362099758f9532680759437c9bf511aafdf
WilliamGanrotYoucal <williamganrot@hotmail.com>
2022-10-26 19:38:48 +0200

    remove commented snippet

--- lib/rate_limiter.ex
+++ lib/rate_limiter.ex
@@ -1,9 +1,3 @@
-# defmodule RateLimiter.Behavior do
-#   @callback allowed_request(term()) :: term()
-#   @callback disallowed_request(term()) :: term()
-#   @optional_callbacks disallowed_request: 1
-# end
-
 defmodule RateLimiter do
   defmacro __using__(opts) do
     quote bind_quoted: [opts: opts] do
